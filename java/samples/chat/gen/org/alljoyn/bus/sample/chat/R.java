ea2087b7bd9c57d8054aea3596e0dc1cdbed3e62
ernielehmanjr <ernie@psu.edu>
2013-01-30 23:18:44 -0500

    UI Updates

--- java/samples/chat/gen/org/alljoyn/bus/sample/chat/R.java
+++ java/samples/chat/gen/org/alljoyn/bus/sample/chat/R.java
@@ -18,33 +18,34 @@ public final class R {
         public static final int ic_tab_use_disabled=0x7f020004;
         public static final int ic_tab_use_enabled=0x7f020005;
         public static final int icon=0x7f020006;
+        public static final int topping_selector=0x7f020007;
     }
     public static final class id {
+        public static final int button1=0x7f050013;
+        public static final int button2=0x7f050004;
+        public static final int button3=0x7f050005;
+        public static final int button4=0x7f050006;
+        public static final int button5=0x7f050007;
+        public static final int button6=0x7f050008;
         public static final int errorDescription=0x7f050000;
         public static final int errorOk=0x7f050001;
-        public static final int hostChannelName=0x7f050002;
-        public static final int hostChannelStatus=0x7f050003;
-        public static final int hostNameCancel=0x7f05000a;
-        public static final int hostNameChannel=0x7f050008;
-        public static final int hostNameOk=0x7f050009;
-        public static final int hostQuit=0x7f050007;
-        public static final int hostSetName=0x7f050004;
-        public static final int hostStart=0x7f050005;
-        public static final int hostStartCancel=0x7f05000c;
-        public static final int hostStartOk=0x7f05000b;
-        public static final int hostStop=0x7f050006;
-        public static final int hostStopCancel=0x7f05000e;
-        public static final int hostStopOk=0x7f05000d;
-        public static final int useChannelName=0x7f05000f;
-        public static final int useChannelStatus=0x7f050010;
-        public static final int useHistoryList=0x7f050014;
-        public static final int useJoin=0x7f050011;
+        public static final int hostNameCancel=0x7f05000b;
+        public static final int hostNameChannel=0x7f050009;
+        public static final int hostNameOk=0x7f05000a;
+        public static final int hostStartCancel=0x7f05000d;
+        public static final int hostStartOk=0x7f05000c;
+        public static final int hostStopCancel=0x7f05000f;
+        public static final int hostStopOk=0x7f05000e;
+        public static final int settingsButton=0x7f050014;
+        public static final int tableRow1=0x7f050012;
+        public static final int textView1=0x7f050002;
+        public static final int useHistoryList=0x7f050011;
         public static final int useJoinCancel=0x7f050016;
         public static final int useJoinChannelList=0x7f050015;
-        public static final int useLeave=0x7f050012;
         public static final int useLeaveCancel=0x7f050018;
         public static final int useLeaveOk=0x7f050017;
-        public static final int useMessage=0x7f050013;
+        public static final int useMessage=0x7f050010;
+        public static final int username=0x7f050003;
     }
     public static final class layout {
         public static final int alljoynerrordialog=0x7f030000;
